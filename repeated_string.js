6bbab6027846f1f1b856da750a0dd236b3d77fb3
hkiame <henrykiame@gmail.com>
2021-08-23 22:46:42 +0300

    quick solution

---
+++ repeated_string.js
@@ -0,0 +1,30 @@
+function repeatedString(s, n){
+    s = s.toString().trim();
+    if(!s.includes("a")){
+        return 0;
+    }
+    if(s.length == 1 && s.includes("a")){
+        return n;
+    }
+
+    var filtered = s.replace(/a/g, "");
+
+    let pack = s.length - filtered.length,
+        packs = n/s.length,
+        rems = n % s.length;
+    pack = Math.floor(packs) * pack;
+
+    if(rems){
+        let subs = s.substring(0, rems);
+
+        filtered = subs.replace(/a/g, "");
+        pack += subs.length - filtered.length;
+
+    }
+
+    return pack;
+}
+let str = "aba";
+let n = 10;
+console.log(repeatedString(str, n));
+
